cfa561427ecf757f3cf1cd1c86e1872ab133f656
Softwarica College Lab <softwarica@softwarica.com>
2019-05-03 13:40:19 +0545

    Initial commit

---
+++ app/src/main/java/model/Word.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Word {
+
+
+    private int wordId;
+    private String word;
+    private String meaning;
+
+// constructor
+    public Word(int wordId, String word, String meaning){
+        this.wordId=wordId;
+        this.word=word;
+        this.meaning=meaning;
+    }
+
+    //getter and setter
+
+
+
+    public int getWordId() {
+        return wordId;
+    }
+
+    public void setWordId(int wordId) {
+        this.wordId = wordId;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+
+
+
+
+
+}
